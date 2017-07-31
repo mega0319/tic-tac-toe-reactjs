d9a55e1d2b0baea8b4fe0f40b00d1ddd4cba1fd9
Naz Khan <nazmuskhan@yahoo.com>
2017-07-31 17:05:28 -0400

    title shadow

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2 className="animated pulse infinite title">Welcome to Tic-Tac-Toe</h2>
+        <h2 className="animated pulse infinite title bubble">Welcome to Tic-Tac-Toe</h2>
         <Board />
       </div>
     );
